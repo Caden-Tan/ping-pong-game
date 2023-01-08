e5431fa7be3234fd0ae168ee1b26e1461a0838ad
Caden Tan <119778802+Caden-Tan@users.noreply.github.com>
2023-01-08 14:48:41 +0800

    pong
    added extra code, paddles move too fast

--- ping_pong_answer.py
+++ ping_pong_answer.py
@@ -24,16 +24,16 @@ class Paddle(GameSprite):
     #method to control the sprite with arrow keys
     def update_right(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_DOWN] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
     def update_left(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_s] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 
 BLUE = (173, 216, 230)
 
@@ -56,14 +56,45 @@ finish = False
 FPS = 60
 clock = time.Clock()
 
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('GREEN PLAYER LOSE!', True, (180, 0, 0))
+lose2 = font.render('BLUE PLAYER LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
 
+    if finish != True:
+        window.fill(BLUE)
+        paddleLeft.update_left()
+        paddleRight.update_right()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(paddleLeft, ball) or sprite.collide_rect(paddleRight, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball.rect.x < 0:
+            finish = False
+            window.blit(lose1, (200, 200))
+
+        if ball.rect.x > win_width:
+            finish = False
+            window.blit(lose2, (200, 200))
+
         paddleLeft.reset()
         paddleRight.reset()
         ball.reset()
 
-        display.update()
-        clock.tick(FPS)
+    display.update()
+    clock.tick(FPS)
